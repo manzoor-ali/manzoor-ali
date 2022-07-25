a86526e6840a611941e18530b87a4d66d905df78
Manzoor <44814649+manzoor-ali@users.noreply.github.com>
2022-07-25 11:07:49 +0500

    Add files via upload

---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import homeSlice from "../components/home/homeSlice";
+
+export const store = configureStore({
+  reducer: {
+    homeData: homeSlice,
+  },
+});
